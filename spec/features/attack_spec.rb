27cc97699f3f8b545e28bd1f27bc1ed04822374d
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-01-10 16:01:49 +0000

    Implementing MVC Model

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,11 @@ feature 'Attacking Player' do
     click_button 'Attack'
     expect(page).to have_content 'Fergus attacked James'
   end
+
+  scenario 'Can reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content 'James: 60 HP'
+    expect(page).to have_content 'James: 50 HP'
+  end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,5 @@
 feature 'Hit points' do
-  scenario 'Player 1 can view player 2 hit points' do
+  scenario 'Player 1 can view Player 2 HP' do
     sign_in_and_play
     expect(page).to have_content 'James: 60 HP'
   end
